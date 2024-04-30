cea38da20c700af6cbad776293d77c1f25011b4c
SigmaZardan <bibekbhujel077@gmail.com>
2024-04-30 12:37:00 +0545

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Bibek Bhujel on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
